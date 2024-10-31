b84bfce1d4992acaa8e48810ca5f0b79085b0694
Wu-Yu-hsien <80038198+Wu-Yu-hsien@users.noreply.github.com>
2024-10-31 16:35:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ var config = {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'Where am I',
             image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'The point shows Pratt Institute Manhattan Campus',
             location: {
-                center: [-122.418398, 37.759483],
+                center: [40.73868577852847, -73.99894206932011],
                 zoom: 8.5,
                 pitch: 60,
                 bearing: 0
